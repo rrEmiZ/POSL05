c06161cff13db70a9fc2e6ca55482c3bf28602ed
Jakub Jońca <jakubjonca@outlook.com>
2021-04-28 18:49:37 +0200

    Lab4

---
+++ POSL051/OsobaExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POSL051
+{
+    public static class OsobaExtension
+    {
+
+        //public static void WypiszOsoby(this List<IOsoba> list)
+        //{
+        //    for (int i = 0; i < list.Count; i++)
+        //    {
+        //        Console.WriteLine($"{i+1}. {list[i].ZwrocPelnaNazwe()}");
+        //    }
+        //}
+
+    }
+}
---
+++ POSL051/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace POSL051
+{
+    public class Fruit
+    {
+        [JsonProperty(PropertyName = "fruit")]
+        public string Type { get; set; }
+        public string Size { get; set; }
+        public string Color { get; set; }
+    }
+
+    public class Indicator
+    {
+        public string id { get; set; }
+        public string value { get; set; }
+    }
+
+    public class Country
+    {
+        public string id { get; set; }
+        public string value { get; set; }
+    }
+
+    public class Statistic
+    {
+        public Indicator indicator { get; set; }
+        public Country country { get; set; }
+        public string value { get; set; }
+        public string @decimal { get; set; }
+        public string date { get; set; }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Statistic> list;
+
+            //Otwieramy stream pliku sample.txt
+            using (var sr = new StreamReader("db.json"))
+            {
+                var json = sr.ReadToEnd();
+
+                list = JsonConvert.DeserializeObject<List<Statistic>>(json);
+            }
+
+
+            //List<Fruit> list;
+
+            ////Otwieramy stream pliku sample.txt
+            //using (var sr = new StreamReader("sample.json"))
+            //{
+            //    var json = sr.ReadToEnd();
+
+            //    list = JsonConvert.DeserializeObject<List<Fruit>>(json);
+            //}
+
+
+            //list.Add(new Fruit()
+            //{
+            //    Color = "Black",
+            //    Type = "Banana",
+            //    Size = "small"
+            //});
+
+
+            //using (var sw = new StreamWriter("sample.json"))
+            //{
+            //    var json = JsonConvert.SerializeObject(list);
+
+            //    sw.WriteLine(json);
+            //}
+
+
+            Console.ReadLine();
+        }
+    }
+}
