4a8dc0e88954c5a9e3d07059f6ae6c3ae6f01299
Jakub Jońca <jakubjonca@outlook.com>
2021-05-05 19:10:13 +0200

    Lab6

--- POSL051/Program.cs
+++ POSL051/Program.cs
@@ -2,127 +2,107 @@
 using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace POSL051
 {
-    public class Function1Exception : ApplicationException
+
+    public class Student
     {
-        public int A { get; set; }
-        
-        public int B { get; set; }
+        public int Id { get; set; }
 
-        public int Result { get; set; }
-
-
-        public Function1Exception() : base() { }
-        public Function1Exception(string message) : base(message) { }
-        public Function1Exception(string message, Exception innerException) : base(message, innerException) { }
-
-
+        public string Nazwisko { get; set; }
     }
-
 
     class Program
     {
         static void Main(string[] args)
         {
-
-            int result;
+            string connectionString = @"Data source=.\SQLExpress;database=programowanieOb;User id=sl05;Password=P@$$w0rd;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            var list = new List<Student>();
             try
             {
-                Divider2(null, 2);
-                Console.WriteLine("Done");
-            }
-            catch (Function1Exception ex)
-            {
-                result = ex.Result;
-                //Console.WriteLine($"Błąd aplikacji w metodzie {ex.Message}, parametry: {ex.A}, {ex.B}");
-                //if (ex.InnerException != null)
-                //{
-                //    Console.WriteLine(ex.InnerException.Message);
-                //}
-            }
-            catch (ApplicationException ex)
-            {
+                connection.Open();
 
-                Console.WriteLine("Błąd aplikacji w metodzie " + ex.Message);
-                if (ex.InnerException != null)
+                //Dodawanie
+     //           {
+     //               var cmd = connection.CreateCommand();
+     //               cmd.CommandText = @"INSERT INTO [dbo].[students]
+     //      ([Nazwisko]
+     //      ,[Imie]
+     //      ,[NrAlbumu]
+     //      ,[Grupa])
+     //VALUES
+     //      (@nazw
+     //      ,@imie
+     //      ,@nrAlb
+     //      ,@gr)";
+     //               cmd.CommandType = CommandType.Text;
+     //               cmd.Parameters.AddWithValue("@nazw", "Jońca");
+     //               cmd.Parameters.AddWithValue("@imie", "Jakub");
+     //               cmd.Parameters.AddWithValue("@nrAlb", "w50038");
+     //               cmd.Parameters.AddWithValue("@gr", "IIDP");
+
+     //               int reslt = cmd.ExecuteNonQuery();
+
+
+
+     //           }
+
+
                 {
-                    Console.WriteLine(ex.InnerException.Message);
+                    SqlCommand sqlCommand = new SqlCommand();
+                    sqlCommand.Connection = connection;
+                    sqlCommand.CommandText = "SELECT Nazwisko, Id FROM students"; // WHERE Id = @param1";
+                    //sqlCommand.Parameters.Add(new SqlParameter("@param1", 2));
+
+                    using (SqlDataReader reader = sqlCommand.ExecuteReader())
+                    {
+
+
+
+                        //Console.WriteLine("Wiersze znajdujące się w tabeli students:");
+                        while (reader.Read())
+                        {
+                            list.Add(new Student()
+                            {
+                                Id = (int)reader["Id"],
+                                Nazwisko = reader["Nazwisko"].ToString()
+                            });
+
+
+                           // Console.WriteLine(
+                           //       reader[0].ToString() + " " +
+                           //     reader["Nazwisko"].ToString() + " " +
+                           //reader["Imie"].ToString() + " " +
+                           //reader["NrAlbumu"].ToString() + " " +
+                           //reader["Grupa"].ToString());
+                        }
+                    }
+
                 }
             }
-            catch (SystemException ex)
+            catch (SqlException e)
             {
-
-                Console.WriteLine(ex.Message);
-            }
-            catch (Exception ex)
-            {
-
-                Console.WriteLine(ex.Message);
+                Console.WriteLine("error");
+                Console.WriteLine(e.Message);
             }
             finally
             {
-                Console.WriteLine("Finally");
+                connection.Close();
+
             }
 
+            foreach (var std in list)
+            {
+                Console.WriteLine($"{std.Id} {std.Nazwisko}");
+            }
 
 
             Console.ReadLine();
         }
 
-        static void Divider2(Nullable<int> a, int? b)
-        {
-            //throw new Exception((a.Value / b.Value).ToString());
-
-
-            try
-            {
-                if (a == null || !a.HasValue)
-                {
-                    throw new NullReferenceException();
-                }
-
-
-                if (b == null)
-                {
-                    throw new Function1Exception("Divider null Value")
-                    {
-                        A = a ?? 0,
-                        B = b ?? 0
-                    };
-                }
-
-
-                int resutl = a.Value / b.Value;
-            }
-            catch (Exception ex)
-            {
-
-                throw new Function1Exception("Divider", ex)
-                {
-                    A = a ?? 0,
-                    B = b ?? 0
-                };
-            }
-        }
-
-
-        static int Divider(int a, int b)
-        {
-            try
-            {
-                return a / b;
-            }
-            catch (Exception ex)
-            {
-
-                throw new Function1Exception("Divider", ex)
-                {
-                    A = a,
-                    B = b
-                };
-            }
-        }
     }
 }
