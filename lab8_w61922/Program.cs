993be269020092a510be3f7e71d8f8f96126b9cb
Dawid <dawid.mielniczek99@gmail.com>
2021-05-25 18:23:02 +0200

    Lab8

---
+++ lab8_w61922/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+
+namespace lab8_w61922
+{
+    public class Car : ICarRepository
+    {
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public int Manufactured { get; set; }
+        public int EngineCapacity { get; set; }
+        public double Power { get; set; }
+        public string Type { get; set; }
+        public string LicensePlate { get; set; }
+
+        public void Add(string _man, string _model, int _Man, int _poj, double _moc, string _typ, string licencja)
+        {
+            var list = new List<Car>();
+            list.Add(new Car()
+            {
+                Manufacturer = _man,
+                Model = _model,
+                Manufactured = _Man,
+                EngineCapacity = _poj,
+                Power = _moc,
+                Type = _typ,
+                LicensePlate = licencja
+            });
+        }
+        public List<Car> GetAll()
+        {
+            List<Car> list;
+            using (var sr = new StreamReader("cars.json"))
+            {
+                var json = sr.ReadToEnd();
+
+                list = JsonConvert.DeserializeObject<List<Car>>(json);
+            }
+            return list;
+        }
+    }
+
+    public interface ICarRepository
+    {
+        List<Car> GetAll();
+        void Add(string _man, string _model, int _Man, int _rok, double _moc, string _typ, string licencja);
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> list;
+            using (var sr = new StreamReader("cars.json"))
+            {
+                var json = sr.ReadToEnd();
+
+                list = JsonConvert.DeserializeObject<List<Car>>(json);
+            }
+            var ileAUDI = list.Where(x => x.Manufacturer == "Audi").Count();
+            var ilePowyzej2L = list.Where(x => x.EngineCapacity > 2000).Count();
+            var ileBMW = list.Where(x => x.Manufacturer == "BMW").Count();
+            var ilePonizej2L = list.Where(x => x.EngineCapacity < 2000).Count();
+
+        }
+    }
+}
---
+++ w61922_zad/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.HSSF.UserModel;
+using System.Reflection;
+
+namespace w61922_zad
+{
+    // zad 1,2
+    class Program
+    {
+        static void GetMethods(object obj)
+        {
+            var type = obj.GetType();
+            var methods = type.GetMethods();
+            foreach (var method in methods)
+            {
+                Console.WriteLine("Metoda" + method.Name + " ,params " + string.Join(",", method.GetParameters().Select(x => x.Name)));
+            }
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+
+        static void GetProperties(object obj)
+        {
+            var type = obj.GetType();
+            var Properties = type.GetProperties();
+            foreach (var property in Properties)
+            {
+                Console.WriteLine("Właściwość " + string.Join(",", property.Name));
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            GetProperties("2d");
+            Console.ReadLine();
+        }
+
+
+    }
+}
