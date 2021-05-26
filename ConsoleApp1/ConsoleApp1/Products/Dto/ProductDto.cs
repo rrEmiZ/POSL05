8c854f5838102cb289742dfe5412ac7c4ba2b645
Jakub Jońca <jakubjonca@outlook.com>
2021-05-26 17:55:26 +0200

    ADO EDM

---
+++ ConsoleApp1/ConsoleApp1/Products/Dto/ProductDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Products.Dto
+{
+    public class ProductDto
+    {
+        public int Id { get;  set; }
+        public string Name { get;  set; }
+        public short ModelYear { get;  set; }
+        public decimal Price { get;  set; }
+        public string BrandName { get;  set; }
+        public int BrandId { get;  set; }
+        public int CategoryId { get;  set; }
+        public string CategoryName { get;  set; }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Products/ProductRepository.cs
@@ -0,0 +1,37 @@
+﻿using ConsoleApp1.Products.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Products
+{
+    public class ProductRepository
+    {
+        private readonly BikeStoresEntities _db;
+
+        public ProductRepository(BikeStoresEntities db)
+        {
+            _db = db;
+        }
+
+        public List<ProductDto> GetProducts()
+        {
+            return _db.products.Select(x => new ProductDto()
+            {
+                Id = x.product_id,
+                Name = x.product_name,
+                ModelYear = x.model_year,
+                Price =  x.list_price,
+                BrandName = x.brand.brand_name,
+                BrandId = x.brand_id,
+                CategoryName = x.category.category_name,
+                CategoryId = x.category_id
+            }).ToList();
+        }
+
+
+
+    }
+}
