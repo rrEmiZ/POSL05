940789f33fec2464285e8961c5d1ebfdc19efc96
Jakub Jońca <jakubjonca@outlook.com>
2021-06-02 17:51:03 +0200

    GetProducts wth errors

--- ConsoleAppCore/ConsoleAppCore/Program.cs
+++ ConsoleAppCore/ConsoleAppCore/Program.cs
@@ -36,6 +36,43 @@ namespace ConsoleAppCore
             Console.ReadLine();
         }
 
-     
+        static List<ProductDto> GetProducts(DateTime date)
+        {
+            using (var db = new BikeStoresContext())
+            {
+                var disconts = db.Discounts.Where(x => x.DateFrom >= date && x.DateTo <= date).ToList();
+
+                var products = db.Products.ToList();
+
+
+                return products.Select(p =>
+                {
+                    var dto = new ProductDto()
+                    {
+                        Name = p.ProductName
+                    };
+
+                    if (disconts.Any(d => d.ProductId == p.ProductId || d.BrandId == p.BrandId || d.CategoryId == p.CategoryId))
+                    {
+                        if (disconts.Any(d => d.ProductId == p.ProductId))
+                        {
+                            var discount = disconts.FirstOrDefault(d => d.ProductId == p.ProductId);
+
+                            dto.Price = p.ListPrice - (p.ListPrice * discount.DicountProcent);
+                        }
+
+                        //Brand ..
+
+
+                        //Category..
+                    }
+
+
+                    return dto;
+                }).ToList();
+
+
+            }
+        }
     }
 }
