8f527fac58c795bb91cc7ee2835bab60fdbbf60f
Jakub Jońca <jakubjonca@outlook.com>
2021-06-16 17:26:08 +0200

    Async

--- ConsoleAppCore/ConsoleAppCore/Program.cs
+++ ConsoleAppCore/ConsoleAppCore/Program.cs
@@ -1,107 +1,139 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using ConsoleAppCore.DbModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace ConsoleAppCore
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            using(var db = new  BikeStoresContext())
+            using (var db = new BikeStoresContext())
             {
-                //var brands = db.Brands.ToList();
+                var discountsTask = GetDiscountsAsync();
 
-                //var brand = brands.FirstOrDefault();
+                var prods = await db.Products.ToListAsync();
 
-                //brand.BrandName = "YYYYY";
-
-                //db.Update(brand);
-
-                //db.SaveChanges();
-
-                //db.Discounts.Add(new Discount()
-                //{
-                //    CategoryId = 1,
-                //    Code = "TEST",
-                //    DicuntValue = .5
-                //});
-
-                //db.SaveChanges();
-
-                using (var trans = db.Database.BeginTransaction( ))
-                {
-                    var brand = new Brand()
-                    {
-                        BrandName = "TESTTTTTTTTTTXXC"
-                    };
-
-                    db.Brands.Add(brand);
-                    db.SaveChanges();
-
-                    var product = new Product()
-                    {
-                        BrandId = brand.BrandId,
-                        CategoryId = 1,
-                        ListPrice = 4,
-                        ModelYear = 1,
-                        ProductName = "Unit tests"
-                    };
-
-                    db.Products.Add(product);
-                    db.SaveChanges();
-
-
-
-                    trans.Commit();
-                }
-
+                var discunts = await discountsTask;
 
             }
-
-
-            Console.WriteLine("test");
-            Console.ReadLine();
         }
 
-        //static List<ProductDto> GetProducts(DateTime date)
-        //{
-        //    using (var db = new BikeStoresContext())
-        //    {
-        //        var disconts = db.Discounts.Where(x => x.DateFrom >= date && x.DateTo <= date).ToList();
 
-        //        var products = db.Products.ToList();
-
-
-        //        return products.Select(p =>
-        //        {
-        //            var dto = new ProductDto()
-        //            {
-        //                Name = p.ProductName
-        //            };
-
-        //            if (disconts.Any(d => d.ProductId == p.ProductId || d.BrandId == p.BrandId || d.CategoryId == p.CategoryId))
-        //            {
-        //                if (disconts.Any(d => d.ProductId == p.ProductId))
-        //                {
-        //                    var discount = disconts.FirstOrDefault(d => d.ProductId == p.ProductId);
-
-        //                    dto.Price = p.ListPrice - (p.ListPrice * discount.DicountProcent);
-        //                }
-
-        //                //Brand ..
-
-
-        //                //Category..
-        //            }
-
-
-        //            return dto;
-        //        }).ToList();
-
-
-        //    }
-        //}
+        static async Task<List<Discount>> GetDiscountsAsync()
+        {
+            using (var db = new BikeStoresContext())
+            {
+                return await db.Discounts.ToListAsync();
+            }
+        }
     }
+
+    //class Program
+    //{
+    //    static async Task Main(string[] args)
+    //    {
+    //        Coffee cup = PourCoffee();
+    //        Console.WriteLine("coffee is ready");
+
+    //        var eggsTask = FryEggsAsync(2);
+    //        var baconTask = FryBaconAsync(3);
+    //        var toastTask = MakeToastWithButterAndJamAsync(2);
+
+    //        var breakfastTasks = new List<Task> { eggsTask, baconTask, toastTask };
+    //        while (breakfastTasks.Count > 0)
+    //        {
+    //            Task finishedTask = await Task.WhenAny(breakfastTasks);
+    //            if (finishedTask == eggsTask)
+    //            {
+    //                Console.WriteLine("eggs are ready");
+    //            }
+    //            else if (finishedTask == baconTask)
+    //            {
+    //                Console.WriteLine("bacon is ready");
+    //            }
+    //            else if (finishedTask == toastTask)
+    //            {
+    //                Console.WriteLine("toast is ready");
+    //            }
+    //            breakfastTasks.Remove(finishedTask);
+    //        }
+
+    //        Console.ReadLine();
+    //    }
+
+    //    static async Task<Toast> MakeToastWithButterAndJamAsync(int number)
+    //    {
+    //        var toast = await ToastBreadAsync(number);
+    //        ApplyButter(toast);
+    //        ApplyJam(toast);
+
+    //        return toast;
+    //    }
+
+    //    private static Juice PourOJ()
+    //    {
+    //        Console.WriteLine("Pouring orange juice");
+    //        return new Juice();
+    //    }
+
+    //    private static void ApplyJam(Toast toast) =>
+    //        Console.WriteLine("Putting jam on the toast");
+
+    //    private static void ApplyButter(Toast toast) =>
+    //        Console.WriteLine("Putting butter on the toast");
+
+    //    private static async Task<Toast> ToastBreadAsync(int slices)
+    //    {
+    //        for (int slice = 0; slice < slices; slice++)
+    //        {
+    //            Console.WriteLine("Putting a slice of bread in the toaster");
+    //        }
+    //        Console.WriteLine("Start toasting...");
+    //        await Task.Delay(2000);
+    //       // Console.WriteLine("Fire! Toast is ruined!");
+    //      //  throw new InvalidOperationException("The toaster is on fire");
+    //      //  await Task.Delay(1000);
+    //        Console.WriteLine("Remove toast from toaster");
+
+    //        return new Toast();
+    //    }
+
+    //    private static async Task<Bacon> FryBaconAsync(int slices)
+    //    {
+    //        Console.WriteLine($"putting {slices} slices of bacon in the pan");
+    //        Console.WriteLine("cooking first side of bacon...");
+    //        await Task.Delay(3000);
+    //        for (int slice = 0; slice < slices; slice++)
+    //        {
+    //            Console.WriteLine("flipping a slice of bacon");
+    //        }
+    //        Console.WriteLine("cooking the second side of bacon...");
+    //        await Task.Delay(3000);
+    //        Console.WriteLine("Put bacon on plate");
+
+    //        return new Bacon();
+    //    }
+
+    //    private static async Task<Egg> FryEggsAsync(int howMany)
+    //    {
+    //        Console.WriteLine("Warming the egg pan...");
+    //        await Task.Delay(3000);
+    //        Console.WriteLine($"cracking {howMany} eggs");
+    //        Console.WriteLine("cooking the eggs ...");
+    //        await Task.Delay(3000);
+    //        Console.WriteLine("Put eggs on plate");
+
+    //        return new Egg();
+    //    }
+
+    //    private static Coffee PourCoffee()
+    //    {
+    //        Console.WriteLine("Pouring coffee");
+    //        return new Coffee();
+    //    }
+    //}
 }
